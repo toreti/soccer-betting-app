4c0c73931f4026c412006f8339bf9a92e84d9761
Felipe Torretto <toreti@gmail.com>
2021-06-27 06:48:18 -0300

    add token component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <token></token>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Token from './components/Token.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Token,
   }
 }
 </script>
---
+++ src/components/Token.vue
@@ -0,0 +1,295 @@
+<template>
+  <fieldset>
+    <legend>Token</legend>
+    <div>
+      <label for="address">Address:</label>
+      <input v-model="address" id="address" disabled/>
+    </div>
+    <div>Owner: <strong>{{ owner }}</strong></div>
+    <div>Total supply: <strong>{{ totalSupply }}</strong></div>
+    <div>Decimals: <strong>{{ decimals }}</strong></div>
+    <div>Symbol: <strong>{{ symbol }}</strong></div>
+    <div>Name: <strong>{{ symbol }}</strong></div>
+  </fieldset>
+</template>
+
+<script>
+import Web3 from 'web3';
+const web3 = new Web3('http://localhost:7545');
+
+const tokenAbi = [
+  {
+    'inputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'constructor'
+  },
+  {
+    'anonymous': false,
+    'inputs': [
+      {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'owner',
+        'type': 'address'
+      },
+      {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'spender',
+        'type': 'address'
+      },
+      {
+        'indexed': false,
+        'internalType': 'uint256',
+        'name': 'value',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'Approval',
+    'type': 'event'
+  },
+  {
+    'anonymous': false,
+    'inputs': [
+      {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'from',
+        'type': 'address'
+      },
+      {
+        'indexed': true,
+        'internalType': 'address',
+        'name': 'to',
+        'type': 'address'
+      },
+      {
+        'indexed': false,
+        'internalType': 'uint256',
+        'name': 'value',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'Transfer',
+    'type': 'event'
+  },
+  {
+    'inputs': [],
+    'name': 'totalSupply',
+    'outputs': [
+      {
+        'internalType': 'uint256',
+        'name': '',
+        'type': 'uint256'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [],
+    'name': 'decimals',
+    'outputs': [
+      {
+        'internalType': 'uint8',
+        'name': '',
+        'type': 'uint8'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [],
+    'name': 'symbol',
+    'outputs': [
+      {
+        'internalType': 'string',
+        'name': '',
+        'type': 'string'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [],
+    'name': 'name',
+    'outputs': [
+      {
+        'internalType': 'string',
+        'name': '',
+        'type': 'string'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [],
+    'name': 'getOwner',
+    'outputs': [
+      {
+        'internalType': 'address',
+        'name': '',
+        'type': 'address'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'account',
+        'type': 'address'
+      }
+    ],
+    'name': 'balanceOf',
+    'outputs': [
+      {
+        'internalType': 'uint256',
+        'name': '',
+        'type': 'uint256'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'recipient',
+        'type': 'address'
+      },
+      {
+        'internalType': 'uint256',
+        'name': 'amount',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'transfer',
+    'outputs': [
+      {
+        'internalType': 'bool',
+        'name': '',
+        'type': 'bool'
+      }
+    ],
+    'stateMutability': 'nonpayable',
+    'type': 'function'
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'owner',
+        'type': 'address'
+      },
+      {
+        'internalType': 'address',
+        'name': 'spender',
+        'type': 'address'
+      }
+    ],
+    'name': 'allowance',
+    'outputs': [
+      {
+        'internalType': 'uint256',
+        'name': '',
+        'type': 'uint256'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function',
+    'constant': true
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'spender',
+        'type': 'address'
+      },
+      {
+        'internalType': 'uint256',
+        'name': 'amount',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'approve',
+    'outputs': [
+      {
+        'internalType': 'bool',
+        'name': '',
+        'type': 'bool'
+      }
+    ],
+    'stateMutability': 'nonpayable',
+    'type': 'function'
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'address',
+        'name': 'sender',
+        'type': 'address'
+      },
+      {
+        'internalType': 'address',
+        'name': 'recipient',
+        'type': 'address'
+      },
+      {
+        'internalType': 'uint256',
+        'name': 'amount',
+        'type': 'uint256'
+      }
+    ],
+    'name': 'transferFrom',
+    'outputs': [
+      {
+        'internalType': 'bool',
+        'name': '',
+        'type': 'bool'
+      }
+    ],
+    'stateMutability': 'nonpayable',
+    'type': 'function'
+  }
+];
+const tokenAddress = '0xb40522068a89111ED350F8aCE4d64eA1ef5ba261';
+const token = new web3.eth.Contract(tokenAbi, tokenAddress);
+
+export default {
+  name: 'Token',
+  data() {
+    return {
+      address: tokenAddress,
+      owner: null,
+      totalSupply: null,
+      decimals: null,
+      symbol: null,
+      name: null,
+    }
+  },
+  mounted() {
+    token.methods.getOwner().call().then(result => this.owner = result);
+    token.methods.totalSupply().call().then(result => this.totalSupply = result);
+    token.methods.decimals().call().then(result => this.decimals = result);
+    token.methods.symbol().call().then(result => this.symbol = result);
+    token.methods.name().call().then(result => this.name = result);
+  }
+}
+</script>
+
+<style>
+</style>
